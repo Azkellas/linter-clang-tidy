5d336cfaf21d971554f3ab147672ee3472f121e3
Azke <e.desbois@hotmail.fr>
2018-05-22 14:58:56 +0200

    Workable skeleton of linter

---
+++ spec/linter-clang-tidy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LinterClangTidyView from '../lib/linter-clang-tidy-view';
+
+describe('LinterClangTidyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
