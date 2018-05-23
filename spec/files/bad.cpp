976b55cbd2c9939a6404e207b78061936142bb3b
Azke <e.desbois@hotmail.fr>
2018-05-23 16:17:50 +0200

    Specs Ok (active / good / bad, #lines only)

---
+++ spec/files/bad.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << endl;
+    for (int i = 0; i < 10; ++i)
+        cout << " i = "  i "\n";
+    return 1;
+}
--- spec/linter-clang-tidy-spec.js
+++ spec/linter-clang-tidy-spec.js
@@ -1,73 +1,50 @@
 'use babel';
 
-import LinterClangTidy from '../lib/linter-clang-tidy';
-
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
 describe('LinterClangTidy', () => {
-  let workspaceElement, activationPromise;
-
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('linter-clang-tidy');
-  });
-
-  describe('when the linter-clang-tidy:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.linter-clang-tidy')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'linter-clang-tidy:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.linter-clang-tidy')).toExist();
-
-        let linterClangTidyElement = workspaceElement.querySelector('.linter-clang-tidy');
-        expect(linterClangTidyElement).toExist();
-
-        let linterClangTidyPanel = atom.workspace.panelForItem(linterClangTidyElement);
-        expect(linterClangTidyPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'linter-clang-tidy:toggle');
-        expect(linterClangTidyPanel.isVisible()).toBe(false);
-      });
+    const { lint } = require('../lib/linter-clang-tidy.js').provideLinter();
+    beforeEach( () => {
+        waitsForPromise( () => {
+            atom.packages.activatePackage('language-c');
+            atom.packages.activatePackage('linter-clang-tidy');
+            atom.config.set('linter-clang-tidy.timeout', 60);
+            atom.config.set('linter-clang-tidy.options', '-checks=*');
+            atom.config.set('linter-clang-tidy.execPath', 'clang-tidy-6.0');
+            return atom.packages.activatePackage('linter-clang-tidy');
+        });
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
 
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
+    it('is in the package list', () =>
+        expect(atom.packages.isPackageLoaded('linter-clang-tidy')).toBe(true));
 
-      expect(workspaceElement.querySelector('.linter-clang-tidy')).not.toExist();
+    it('is active', () =>
+        expect(atom.packages.isPackageActive('linter-clang-tidy')).toBe(true));
 
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'linter-clang-tidy:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let linterClangTidyElement = workspaceElement.querySelector('.linter-clang-tidy');
-        expect(linterClangTidyElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'linter-clang-tidy:toggle');
-        expect(linterClangTidyElement).not.toBeVisible();
-      });
+    it('checks bad.cpp', () => {
+        waitsForPromise(() => {
+            filename = __dirname + '/files/bad.cpp';
+            return atom.workspace.open(filename).then(editor => {
+                return lint(editor).then(messages => {
+                    expect(messages.length).toBe(5);
+                });
+            });
+        });
     });
-  });
+
+    it('checks good.cpp', () => {
+        waitsForPromise(() => {
+            filename = __dirname + '/files/good.cpp';
+            return atom.workspace.open(filename).then(editor => {
+                return lint(editor).then(messages => {
+                    expect(messages.length).toBe(0);
+                });
+            });
+        });
+    });
+
 });
